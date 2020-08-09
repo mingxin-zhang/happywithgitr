57612a286087f7d83e06b2489e1ec6b051ec802e
mingxinzhang <746548383@qq.com>
2020-08-09 15:52:43 +0800

    ccc

---
+++ test1.R
@@ -0,0 +1,10 @@
+#'aslhdkjagslkajsf
+## here is a  regular code comment ,that will remain as such
+summary(VADeaths)
+
+#' d;aksdl;kasd;lkal;skd;lafksasf
+#' asdasd
+#' asdasdasd
+#' asdasdasd
+#+ dotchar
+dotchart(VADeaths,main = "Death Rates in Virginia -1940")
